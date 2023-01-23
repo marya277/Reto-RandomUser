7283a4ee36010dc78a132db06459e58b33e51778
marya277 <maryarroyo85@gmail.com>
2023-01-23 12:07:15 -0500

    feat: styles header and cards

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,10 @@ const AppUser = () => {
     <div className="container-fluid p-0">
       <Header/>
     
-    <div className="row container-fluid d-flex justify-content-center mt-5">
+    <div className="row container-fluid d-flex justify-content-center my-5 mx-auto">
       {users.map((user) => (
-        <div key={user.id.value} className="col-md-4 ">
-          <div className="card profile-card-3">
+        <div key={user.id.value} className="  col sm -6 col-md-4 ">
+          <div className="card profile-card-3 my-5 mx-auto">
             <div className="background-block">
               <img
                 src="https://images.unsplash.com/photo-1619252584172-a83a949b6efd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
@@ -42,13 +42,13 @@ const AppUser = () => {
             <div className="profile-thumb-block">
               <img src={user.picture.medium} alt="img" className="profile mb-5" />
             </div>
-            <div className="card-content">
+            <div className=" card-body card-content">
               <p className="text-md">{user.name.first} {user.name.last}</p>
               <p className="text-sm">Género: {user.gender}</p>
               <small><p className="text-sm"> Correo: {user.email}</p></small>
 
               <p className="text-sm font-serif">País: {user.location.country}</p>
-              <div className="icon-block mb-3">
+              <div className="card-footer icon-block mb-5">
                 <a href="#"> <i className="fa fa-facebook"></i>
                 </a>
                 <a href="#"><i className="fa fa-twitter"></i>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import React from 'react'
       <div className="container-fluid mb-3 head">
     <div className="row h-100 align-items-center">
       <div className="col-12 text-center title-container">
-        <h2 className="fw-light my-4">Reto Kamelia- Consulta de usuarios  Random user Api</h2>
+        <h2 className="fw-light my-4 head-text">Reto Kamelia- Consulta de usuarios  Random user Api</h2>
         
       </div>
     </div>
